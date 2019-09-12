06cdda42df649d77d02fe37b6bce4a80ec28d023
geryan <geryan@users.noreply.github.com>
2019-09-12 06:21:53 +0000

    fit sdms in  tibble

--- R/09_fit_distribution_models.R
+++ R/09_fit_distribution_models.R
@@ -6,6 +6,8 @@ library(sf)
 library(lubridate)
 library(purrr)
 library(tibble)
+library(future)
+library(furrr)
 
 load(file = "output/RData/00_comp_controls.RData")
 load(file = "output/RData/01_landscape_variables.RData")
@@ -16,21 +18,30 @@ source.functions("R/functions")
 
 # LBP -----------------
 
-brt_lb <- tibble(year.from = c(80, 09),
+plan(multisession, workers = 8)
+
+brt_lb <- tibble(year.from = c("80", "09"),
                  bg = c(TRUE, FALSE),
                  varset = list(vn_lb_1, vn_lb_2)) %>%
   expand(year.from, bg, varset) %>%
   rowwise %>%
-  mutate(pa.data = list(get(sprintf("pa_lb_%02d%s", year.from, ifelse(bg, "", "x"))))) %>%
+  mutate(pa.data = list(get(sprintf("pa_lb_%S%s", year.from, ifelse(bg, "", "x"))))) %>%
   ungroup %>%
-  mutate(model.data = map(.x = pa.data,
+  mutate(model.data = future_map(.x = pa.data,
                           .f = get.model.data,
                           y = vars_1_01,
                           na.omit = FALSE),
-         model.data = map2(.x = model.data,
+         model.data = future_map2(.x = model.data,
                            .y = varset,
                            .f = clean.model.data)) %>%
-  mutate(brt.fit = map(.x = model.data,
+  mutate(brt.fit = future_map(.x = model.data,
                        .f = gbmstep,
                        learning.rate = 0.01,
-                       bag.fraction = 0.75))
+                       bag.fraction = 0.75)) %>%
+  mutate(auc = future_map(.x = brt.fit,
+                   .f = brt_auc))
+
+save(
+  brt_lb,
+  file = "output/RData/09_fit_distribution_models.RData"
+)
--- junk.R
+++ junk.R
@@ -1,62 +1,12 @@
-xx <- foreach(i = 1:3) %:%
-        foreach(j = c(100, 1000, 10000)) %do% {
-                i+j
-        }
 
-h100 <- lv_1_01[[1]][[18]]
-h125 <- lv_1_01[[26]][[18]]
-h150 <- lv_1_01[[51]][[18]]
+plot(ip_lb_09_1, col = viridis(100, option = "A"))
+plot(pa_lb_09["PA"], add = TRUE, col = viridis(2, option = "A"))
 
-h400 <- lv_4_01[[1]][[18]]
-h425 <- lv_4_01[[26]][[18]]
-h450 <- lv_4_01[[51]][[18]]
+ggplot()+
+  geom_raster(data = ip_lb_09_1 %>% as.data.frame(., xy = TRUE), aes(x = x, y = y, fill = layer))
 
-h800 <- lv_4_01[[1]][[18]]
-h825 <- lv_4_01[[26]][[18]]
-h850 <- lv_4_01[[51]][[18]]
+plot(pa_lb_09["PA"], col = viridis(2, option = "A"))
+
+plot(pa_lb_09["PA"])
 
 
-lp(h100, h125, h150)
-
-lp(h400, h425, h450)
-
-
-lp(h800, h825, h850)
-
-
-f100 <- lv_1_01[[1]][[19]]
-f125 <- lv_1_01[[26]][[19]]
-f150 <- lv_1_01[[51]][[19]]
-
-f400 <- lv_4_01[[1]][[19]]
-f425 <- lv_4_01[[26]][[19]]
-f450 <- lv_4_01[[51]][[19]]
-
-f800 <- lv_4_01[[1]][[19]]
-f825 <- lv_4_01[[26]][[19]]
-f850 <- lv_4_01[[51]][[19]]
-
-aa <- h150
-
-aa[aa < 5] <- NA
-
-
-aa <- rst.op(input1 = h125, op = "pb", proj_mask = ch_mask, filename = "test.grd", layernames = "pb")
-
-bb  <- rst.op(input1 = h125, op = "harvest", proj_mask = ch_mask, filename = "test2.grd", layernames = "logging")
-
-lapply(get(sprintf("lv_%s_%s",
-                   scn_list[i],
-                   rep_list[j])),
-       FUN = function(x){x[["harvest"]]})
-
-
-aa <- get.dist(fire_history = ch_fire_history,
-               logging_history = ch_logging_history,
-               fs = lapply(lv_1_01, FUN = function(x){x[["firesev"]]}),
-               ha = lapply(lv_1_01, FUN = function(x){x[["harvest"]]}),
-               out_path = "output/landscape_vars",
-               scn_id = "1_01",
-               proj_mask = ch_mask,
-               timesteps = 5,
-               year0 = 2019)
