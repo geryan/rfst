139bc60d577dcdfddff9464c59c46e73fbb6311f
geryan <geryan@users.noreply.github.com>
2019-09-12 07:44:50 +0000

    predict brts

--- R/09_fit_distribution_models.R
+++ R/09_fit_distribution_models.R
@@ -8,13 +8,22 @@ library(purrr)
 library(tibble)
 library(future)
 library(furrr)
+library(tidyr)
 
 load(file = "output/RData/00_comp_controls.RData")
 load(file = "output/RData/01_landscape_variables.RData")
 load(file = "output/RData/02_species_occurrences.RData")
 load(file = "output/RData/07_combined_variables.RData")
 
+
 source.functions("R/functions")
+
+
+
+vn_lb_1 <- c("lbm_prop", "lbm_biom", "prop_old_150", "prop_oge_3h", "biom_oge_3h", "prec01", "prec07", "tmax01", "tmin07", "lvdaw", "lvdma", "lvdmi", "lvdsw", "ahr", "tho", "max_age")
+
+vn_lb_2 <- c("prec01", "prec07", "tmax01", "tmin07", "lvdaw", "lvdma", "lvdmi", "lvdsw", "ahr", "tho", "max_age")
+
 
 # LBP -----------------
 
@@ -25,7 +34,7 @@ brt_lb <- tibble(year.from = c("80", "09"),
                  varset = list(vn_lb_1, vn_lb_2)) %>%
   expand(year.from, bg, varset) %>%
   rowwise %>%
-  mutate(pa.data = list(get(sprintf("pa_lb_%S%s", year.from, ifelse(bg, "", "x"))))) %>%
+  mutate(pa.data = list(get(sprintf("pa_lb_%s%s", year.from, ifelse(bg, "b", "x"))))) %>%
   ungroup %>%
   mutate(model.data = future_map(.x = pa.data,
                           .f = get.model.data,
@@ -39,7 +48,8 @@ brt_lb <- tibble(year.from = c("80", "09"),
                        learning.rate = 0.01,
                        bag.fraction = 0.75)) %>%
   mutate(auc = future_map(.x = brt.fit,
-                   .f = brt_auc))
+                   .f = brt_auc))%>%
+  unnest(auc)
 
 save(
   brt_lb,
