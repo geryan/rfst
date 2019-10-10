d1e8b5589050039dad7b9caf642ad8f550d33b9e
geryan <geryan@users.noreply.github.com>
2019-10-10 05:32:18 +0000

    carrying capacity funtion cc_6.R

---
+++ R/functions/cc_6.R
@@ -0,0 +1,14 @@
+cc_6 <- function (landscape, timestep) {
+  
+  fun <- function(suitability) {
+    6 - round(6 * dlogis(suitability, scale = 0.25))
+  }
+  
+  suit <- landscape$suitability
+  if (raster::nlayers(suit) > 1) {
+    suit <- suit[[timestep]]
+  }
+  
+  calc(suit, fun)
+  
+}
