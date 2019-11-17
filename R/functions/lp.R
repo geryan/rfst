a8e125e111e939b991a7d21abe7025c1ba7ed131
geryan <geryan@users.noreply.github.com>
2019-11-17 11:47:38 +0000

    added twocol arg to lp illustrative completed

--- R/functions/lp.R
+++ R/functions/lp.R
@@ -1,12 +1,21 @@
-lp <- function(x, ...){
+lp <- function(x, twocol = FALSE, ...){
   
   library(rasterVis)
   
-  if(missing(...)){
-    z <- levelplot(x, margin = FALSE)
-  } else {
-    z <- levelplot(stack(x, ...), margin = FALSE)
+  if(twocol){
+    if(missing(...)){
+      z <- levelplot(x, col.regions = magma(n = 16)[c(1,11)], at = c(0, 0.5, 1), margin = FALSE)
+    } else {
+      z <- levelplot(stack(x, ...), col.regions = magma(n = 16)[c(1,11)], at = c(0, 0.5, 1), margin = FALSE)
+    }
+  }else{
+    if(missing(...)){
+      z <- levelplot(x, margin = FALSE)
+    } else {
+      z <- levelplot(stack(x, ...), margin = FALSE)
+    }  
   }
+  
   
   return(z)
   
