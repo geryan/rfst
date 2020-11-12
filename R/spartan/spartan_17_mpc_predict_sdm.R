85d7094e273e4c445ba8c406634c93051c4a39d2
Gerry Ryan <geryan@users.noreply.github.com>
2020-11-12 21:48:20 +1100

    pred mpc sdm failsafe not overwrite complted bricks

--- R/spartan/spartan_17_mpc_predict_sdm.R
+++ R/spartan/spartan_17_mpc_predict_sdm.R
@@ -32,27 +32,52 @@ j <- as.numeric(command_args[2])
 
 pred_out_path <- "/data/scratch/projects/punim0995/habitat_pred_mpc/"
 
+file_list <- list.files("/data/scratch/projects/punim0995/habitat_pred_mpc/")
 
-pred <- brtpredict(
-  variables = varset_mpc$all_vars[[i]],
-  model = sdm_results_mpc_ch$brt.fit[[j]],
-  out_path = pred_out_path,
-  scn_id = varset_mpc$cscnid[[i]], # NB needs cscnid not scn_id if multiple climate scenarios with each scenario
-  varset = "",
-  species = sdm_results_mpc_ch$sp[[j]],
-  initial = FALSE,
-  pll = FALSE,
-  ncores = 1
-)
-
-
-
-saveRDS(
-  object = pred,
-  file = sprintf(
-    "%s/pred_mpc_%s_%s.Rds",
-    "/data/gpfs/projects/punim0995/rfst/output/spartan_RData/mpc_pred",
-    varset_mpc$cscnid[[i]], # NB needs cscnid not scn_id if multiple climate scenarios with each scenario
-    sdm_results_mpc_ch$sp[[j]]
+if(
+  any(
+    file_list == sprintf(
+      "brtpred_%s_%s_%s.grd",
+      varset_mpc$cscnid[[i]],
+      "",
+      sdm_results_mpc_ch$sp[[j]]
+    )
   )
-)
+){
+  
+  print("exists")
+  
+  # this if else allows it to skip ones that are already made and do ones that still need to be done
+  
+} else {
+  
+  pred <- brtpredict(
+    variables = varset_mpc$all_vars[[i]],
+    model = sdm_results_mpc_ch$brt.fit[[j]],
+    out_path = pred_out_path,
+    scn_id = varset_mpc$cscnid[[i]], # NB needs cscnid not scn_id if multiple climate scenarios with each scenario
+    varset = "",
+    species = sdm_results_mpc_ch$sp[[j]],
+    initial = FALSE,
+    pll = FALSE,
+    ncores = 1
+  )
+  
+  
+  
+  saveRDS(
+    object = pred,
+    file = sprintf(
+      "%s/pred_mpc_%s_%s.Rds",
+      "/data/gpfs/projects/punim0995/rfst/output/spartan_RData/mpc_pred",
+      varset_mpc$cscnid[[i]], # NB needs cscnid not scn_id if multiple climate scenarios with each scenario
+      sdm_results_mpc_ch$sp[[j]]
+    )
+  )
+  
+}
+
+  
+  
+any(file_list == sprintf("brtpred_%s_%s_%s.grd",varset_mpc$cscnid[[i]],"", sdm_results_mpc_ch$sp[[j]]))
+
