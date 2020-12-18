cb67fbfd0f37ff699ca9dd367e9409f3e2ea2e48
Gerry Ryan <geryan@users.noreply.github.com>
2020-12-18 18:11:27 +1100

    metapop capacity pevo eg and ch

---
+++ R/spartan/spartan_13.1_pva_mpc5_pevo.R
@@ -0,0 +1,124 @@
+# 13 metapopulation capacity models data list for PVA species
+
+source("R/spartan/spartan_settings.R")
+
+
+library(dplyr)
+library(purrr)
+library(tibble)
+library(tidyr)
+library(raster)
+library(sp)
+library(sf)
+library(magrittr)
+library(igraph)
+library(metacapa)
+
+load(file = "output/RData/00_controls.RData")
+load(file = "output/RData/01_landscape_variables.RData")
+load(file = "output/RData/04.3_tsl_aggregated.RData")
+load(file = "output/RData/10_predict_SDMs_2.RData")
+load(file = "output/RData/13.0_pva_mpc_list.RData")
+
+source.functions("R/functions")
+
+
+
+command_args <- commandArgs(trailingOnly = TRUE)
+
+i <- as.numeric(command_args[1])
+
+
+# threshold_max_sss_pevo <- get.threshold.max.sss(
+#   x = pred_set2$predmaps[[1]],
+#   y = pa_data,
+#   sp = "pevo",
+#   plot_roc = TRUE,
+#   plot_sss = TRUE
+# )
+
+threshold_max_sss_pevo <- 0.514
+
+
+mpcdat <- inner_join(
+  mpc_dat_pva %>%
+    dplyr::select(-aggmaps),
+  agg_set2
+)
+
+agg_map <- mpcdat$aggmaps[[i]]
+
+tsl <- tsl_agg_ch$tsl_agg5[[which(tsl_agg_ch$scn_id == mpcdat$scn_id[i])]]
+
+species_dat <- tribble(
+  ~sp,    ~habfun,
+  "gyle", habitat.downupfun,
+  "pevo", habitat.upfun,
+  "peau", habitat.upfun,
+  "smle", NA,
+  "tyte", habitat.downupfun,
+  "vava", NA
+)
+
+habfun <- species_dat$habfun[[which(species_dat$sp == mpcdat$sp[i])]]
+
+if(!is.na(habfun)){
+  hd_vals <- getValues(tsl)
+  
+  hd_vals_mod <- apply(
+    X = hd_vals,
+    FUN = function(x){
+      if(is.na(x)){
+        return(1)
+      } else {
+        return(habfun(x))
+      }
+    },
+    MARGIN = c(1,2)
+  )
+  
+  agg_vals <- getValues(agg_map)
+  
+  hab_vals <- agg_vals * hd_vals_mod
+  
+  agg_map[] <- hab_vals
+}
+
+
+dispfun <- dispersal_negexp(1/mpcdat$ddist[i])
+
+patch <- patmat(
+  x = agg_map,
+  threshold = threshold_max_sss_pevo,
+  write = FALSE
+)
+
+mpc <- metacapstack(
+  x = patch,
+  f = dispfun,
+  year0 = year0
+) %>%
+  rename(
+    mpc = metapopulation_capacity
+  )
+
+
+
+
+mpc_results_pva <- bind_cols(
+  mpcdat[i,] %>%
+    dplyr::select(-aggmaps),
+  mpc
+)
+
+
+saveRDS(
+  object = mpc_results_pva,
+  file = sprintf(
+    "%s/mpc_pvap5_%s_%s.Rds",
+    "/data/gpfs/projects/punim0995/rfst/output/spartan_RData/mpc_pva",
+    mpcdat$cscnid[i], # NB needs cscnid not scn_id if multiple climate scenarios with each scenario
+    mpcdat$sp[i]
+  )
+)
+
